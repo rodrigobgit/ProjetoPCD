eb5a5781bbf63b77fe972281b96c76f9c05c56c3
rodrigobgit <56572500+rodrigobgit@users.noreply.github.com>
2022-11-08 21:53:05 +0000

    Add files via upload

---
+++ Projeto2022Agar.io_Base2/src/game/BotPlayer.java
@@ -0,0 +1,18 @@
+package game;
+
+public class BotPlayer extends Player {
+	public BotPlayer(int id, Game game, byte strength) {
+		super(id, game, strength);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public boolean isHumanPlayer() {
+		return false;
+	}
+}
